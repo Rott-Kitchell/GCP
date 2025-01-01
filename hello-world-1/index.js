713024d683dc78fd16f5c46dc45fa8a57180ffcb
Rott Kitchell <r.kitchell815@gmail.com>
2024-12-31 23:08:17 -0600

    adding old code to new start

--- hello-world-1/index.js
+++ hello-world-1/index.js
@@ -1,21 +1,26 @@
 const functions = require("@google-cloud/functions-framework");
 
-functions.http("BChandler", (req, res) => {
-  let order = req.body;
+functions.http("BChandler", (event) => {
+  let order = event.body;
   console.log("🚀 ~ file: index.js:5 ~ http ~ order:", order);
 
-  if (
-    typeof order.scope !== "undefined" &&
-    order.scope == "store/order/statusUpdated"
-  ) {
+  if (order.scope == "store/order/statusUpdated") {
     console.log(
       `*~*~*~*~*~*Order ${order.data.id} is heading from BC to Monday!*~*~*~*~*~*`
     );
-    res.send("OK");
-    //return orderUpdated(order);
+    return orderUpdated(order);
   } else {
     // res.status(200).send();
     // console.log(order.scope);
-    res.status(200).send("OK");
+    return {
+      statusCode: 200,
+      body: JSON.stringify(order.scope),
+    };
   }
 });
+
+const orderUpdated = async ({ data }) => {
+  let orderId = data.id;
+  const fullOrder = await getOrderInfo(orderId);
+  return BCToMondayProcessor(fullOrder);
+};
