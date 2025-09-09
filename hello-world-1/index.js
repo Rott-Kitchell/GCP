a71c1efb056783717e93bc1fd79630dd2470620b
Rott Kitchell <r.kitchell815@gmail.com>
2025-09-09 15:03:39 -0500

    res.status(200);

--- hello-world-1/index.js
+++ hello-world-1/index.js
@@ -1,25 +1,26 @@
 import { http } from "@google-cloud/functions-framework";
 
-http("BChandler", (event) => {
-  const pubSubMessage = event.body.message;
+http("BChandler", (req, res) => {
+  const pubSubMessage = req.body.message;
   const order = pubSubMessage.data
     ? Buffer.from(pubSubMessage.data, "base64").toString().trim()
     : "World";
   console.log("🚀 ~ file: index.js:5 ~ http ~ order:", order);
 
-  if (order.scope == "store/order/statusUpdated") {
-    console.log(
-      `*~*~*~*~*~*Order ${order.data.id} is heading from BC to Monday!*~*~*~*~*~*`
-    );
-    return orderUpdated(order);
-  } else {
-    // res.status(200).send();
-    // console.log(order.scope);
-    return {
-      statusCode: 200,
-      body: JSON.stringify(order.scope),
-    };
-  }
+  // if (order.scope == "store/order/statusUpdated") {
+  //   console.log(
+  //     `*~*~*~*~*~*Order ${order.data.id} is heading from BC to Monday!*~*~*~*~*~*`
+  //   );
+  //   return orderUpdated(order);
+  // } else {
+  //    res.status(200).send();
+  //   // console.log(order.scope);
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify(order.scope),
+  //   };
+  // }
+  res.status(200);
 });
 
 const orderUpdated = async ({ data }) => {
